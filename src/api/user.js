29dec7e307977492afa81a40d0b9b46f30772180
TieZhuGieGie <dcx1378832571@163.com>
2024-12-21 17:51:39 +0800

    TieZhuDing

--- src/api/user.js
+++ src/api/user.js
@@ -6,3 +6,13 @@ export function Login(username,password){
     data.password = password;
     return post("/user/login/",data);
 }
+
+export function Register(username,password,email,organization,userType){
+    let data = {};
+    data.username = username;
+    data.password = password;
+    data.email = email;
+    data.organization = organization;
+    data.userType = userType;
+    return post("/user/register/",data);
+}
